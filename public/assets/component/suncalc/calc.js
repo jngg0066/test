b80e5552d1b77bdde3982b10555c0be294cacc6c
jngg0066 <jngg0066@student.monash.edu>
2024-03-14 16:02:26 +1100

    test

--- public/assets/component/suncalc/calc.js
+++ public/assets/component/suncalc/calc.js
@@ -1,20 +1,20 @@
 function calculateSunscreen() {
     const uvLevel = parseInt(document.getElementById('uv-level').value);
     let sunscreenRecommendation = '';
-
+ 
     if (uvLevel >= 1 && uvLevel <= 2) {
         sunscreenRecommendation = 'Use SPF 15+ sunscreen.';
     } else if (uvLevel >= 3 && uvLevel <= 5) {
-        sunscreenRecommendation = 'Use SPF 30+ sunscreen, and wear protective clothing, a wide-brimmed hat, and sunglasses.';
+        sunscreenRecommendation = '<li>Use SPF 30+ sunscreen.</li> <li>wear protective clothing</li><li> a wide-brimmed hat</li><li> wear sunglasses.</li>';
     } else if (uvLevel >= 6 && uvLevel <= 7) {
-        sunscreenRecommendation = 'Use SPF 30+ sunscreen, and wear protective clothing, a wide-brimmed hat, and sunglasses.';
+        sunscreenRecommendation = '<li>Use SPF 30+ sunscreen</li><li> wear protective clothing</li><li>a wide-brimmed hat</li><li>wear sunglasses.</li>';
     } else if (uvLevel >= 8 && uvLevel <= 10) {
-        sunscreenRecommendation = 'Use SPF 30+ sunscreen, and avoid the sun between 10 a.m. and 4 p.m.';
+        sunscreenRecommendation = '<li>Use SPF 30+ sunscreen</li><li>avoid the sun between 10 a.m. and 4 p.m.</li>';
     } else if (uvLevel >= 11) {
         sunscreenRecommendation = 'Stay indoors.';
     } else {
         sunscreenRecommendation = 'Invalid UV level. Please enter a number between 1 and 11.';
     }
-
+ 
     document.getElementById('sunscreen-output').innerHTML = sunscreenRecommendation;
 }
