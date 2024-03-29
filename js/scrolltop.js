db5c8dfa7108648c47d5a38e03d064d460c5a8fe
jngg0066 <jngg0066@student.monash.edu>
2024-03-29 23:10:46 +1100

    added scroll up button
    added scroll up button

---
+++ js/scrolltop.js
@@ -0,0 +1,18 @@
+// Corrected JavaScript code in scrolltop.js
+
+let mybutton = document.getElementById("scrollupbtn"); // Updated to match the button ID in HTML
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
