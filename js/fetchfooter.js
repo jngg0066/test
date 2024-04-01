64cd848595a899f70fe0b21beca782eae80c567f
jngg0066 <jngg0066@student.monash.edu>
2024-04-01 20:07:27 +1100

    moved files

--- js/fetchfooter.js
+++ js/fetchfooter.js
@@ -1,14 +1,11 @@
-// fetchfooter.js
-// Fetch footer.html using JavaScript
+
 fetch('../assets/header-footer/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
-        // After inserting the footer content, load the footer CSS
-        loadCSS('../css/footer.css');
+        loadCSS('../css/general/footer.css');
     });
 
-// Function to dynamically load CSS
 function loadCSS(filename) {
     var head = document.head;
     var link = document.createElement('link');
--- js/fetchheader.js
+++ js/fetchheader.js
@@ -1,14 +1,12 @@
-// fetchHeader.js
-// Fetch header.html using JavaScript
+
 fetch('../assets/header-footer/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
-        // After inserting the header content, load the header CSS
-        loadCSS('../css/header.css');
+        loadCSS('../css/general/header.css');
     });
 
-// Function to dynamically load CSS
+
 function loadCSS(filename) {
     var head = document.head;
     var link = document.createElement('link');
