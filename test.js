60da97aec14eded5d93b33e4f21757fba4704822
jngg0066 <jngg0066@student.monash.edu>
2024-04-18 23:10:20 +1000

    added stuff into test

--- test.js
+++ test.js
@@ -57,3 +57,19 @@ document.getElementById('calc-entry-year-btn').addEventListener('click', functio
   updateOutput(primarySchoolEntryYear, "primary-school-years");
   updateOutput(highSchoolEntryYear, "high-school-years");
 });
+
+    // JavaScript to handle collapsible functionality
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        } 
+      });
+    }
